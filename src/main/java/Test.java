01cb5a8e39b90535898a45f5aad00a1239789b36
Xuejianmin666 <821665463@qq.com>
2022-10-12 11:28:45 +0800

    在vi中提交

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("hellWord");
-        System.out.println("6666666666666666 5555555555v1");
+        System.out.println("6666666666666666 5555555555v1555");
     }
 }
